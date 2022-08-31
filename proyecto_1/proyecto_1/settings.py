e0227def6fa5a6f2c61aa505b404c59b9c23c1ce
campeone-pixel <posesmatiasabel@gmail.com>
2022-08-30 22:12:09 -0300

    ahi guarde las plantillas

--- proyecto_1/proyecto_1/settings.py
+++ proyecto_1/proyecto_1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'proyecto_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Users/Mati/Documents/Python Scripts/MVT+MatiasPoses/templates'],
+        'DIRS': ['C:/Users/Mati/Documents/Python Scripts/MVT+MatiasPoses/proyecto_1/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
